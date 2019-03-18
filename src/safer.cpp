c9ca6e2470afe66542c2dbdbfe83611cb5b2dbe0
Masashi Fujita <objectxtreme@gmail.com>
2019-03-18 22:11:27 +0900

    feat: Use `conan`

--- src/safer.cpp
+++ src/safer.cpp
@@ -1,83 +1,75 @@
-/* -*- mode: C++ -*- */
-/*
- * safer.cpp:
- *
- * Copyright (c) 2015 Masashi Fujita
- *
- */
+#include "safer.hpp"
 
 #include <cstring>
 #include <cstdlib>
 #include <algorithm>
-#include "safer.hpp"
 
 #ifdef HAVE_CONFIG_H
 #   include "config.h"
 #endif
 
-#define NUM_ELEMENTS(X_)        (sizeof (X_) / sizeof (*(X_)))
-
-////////////////////////////////////////////////////////////////////////
 
 namespace Safer {
+    namespace {
+        inline uint8_t rotl (uint8_t val, size_t count) {
+            return static_cast<uint8_t> ((val << count) | (val >> (8 - count)));
+        }
 
-    static inline uint8_t       rotl (uint8_t val, size_t count) {
-        return static_cast<uint8_t>((val << count) | (val >> (8 - count))) ;
-    }
+        /**
+         * The pseudo-hadamard transformation...
+         * [X' Y'] = [X Y][2 1]
+         *                [1 1]
+         */
+        inline void pht (uint8_t& x, uint8_t& y) {
+            y = x + y;
+            x = x + y;
+        }
 
-    /**
-     * The pseudo-hadamard transformation...
-     * [X' Y'] = [X Y][2 1]
-     *                [1 1]
-     */
-    static inline void  pht (uint8_t &x, uint8_t &y) {
-        y = x + y ;
-        x = x + y ;
-    }
+        /**
+         * The inverted pseudo-hadamard transformation...
+         * [X' Y'] = [X Y][ 1 -1]
+         *                [-1  2]
+         */
+        inline void ipht (uint8_t& x, uint8_t& y) {
+            x = x - y;
+            y = y - x;
+        }
 
-    /**
-     * The inverted pseudo-hadamard transformation...
-     * [X' Y'] = [X Y][ 1 -1]
-     *                [-1  2]
-     */
-    static inline void  ipht (uint8_t &x, uint8_t &y) {
-        x = x - y ;
-        y = y - x ;
-    }
+        void BuildKeyBlock (block_t result[2], const void* key, size_t key_size) {
+            std::memset (result, 0, 2 * sizeof (block_t));
 
-    static void BuildKeyBlock (block_t result [2], const void *key, size_t key_size) {
-        std::memset (result, 0, 2 * sizeof (block_t)) ;
-
-        if (key_size <= sizeof (block_t)) {
-            const uint8_t *     p = static_cast<const uint8_t *> (key) ;
-            for (size_t i = 0 ; i < key_size ; ++i) {
-                result [0][i] = p [i] ;
+            if (key_size <= sizeof (block_t)) {
+                auto p = static_cast<const uint8_t*> (key);
+                for (size_t i = 0; i < key_size; ++i) {
+                    result[0][i] = p[i];
+                }
+                std::memcpy (result[1], result[0], sizeof (block_t));
             }
-            std::memcpy (result [1], result [0], sizeof (block_t)) ;
-        }
-        else {
-            std::memcpy (result, key, std::min (key_size, 2 * sizeof (block_t))) ;
+            else {
+                std::memcpy (result, key, std::min (key_size, 2 * sizeof (block_t)));
+            }
         }
     }
 
-    Key::Key (const Table &tab, const void *key, size_t key_size, size_t nRounds)
+    Key::Key (const Table &tab, const void *key, size_t key_size, size_t nRounds) noexcept
             : nRounds_ (std::min<size_t> (nRounds, MAX_NUM_ROUNDS)) {
         block_t K [2] ;
         BuildKeyBlock (K, key, key_size) ;
         Initialize (tab, K [0], K [1]) ;
     }
 
-    Key::Key (const Key &src) : nRounds_ (src.nRounds_) {
+    Key::Key (const Key &src) noexcept
+            : nRounds_ (src.nRounds_) {
         std::memcpy (values_, src.values_, sizeof (values_)) ;
     }
 
-    Key &       Key::Assign (const Key &src) {
+    Key &       Key::Assign (const Key &src) noexcept {
         nRounds_ = src.nRounds_ ;
         std::memcpy (values_, src.values_, sizeof (values_)) ;
         return *this ;
     }
 
-    void        Key::Initialize (const Table &tab, const block_t &key1, const block_t &key2) {
+    void        Key::Initialize (const Table &tab, const block_t &key1, const block_t &key2) noexcept {
         uint8_t ka [BLOCK_LEN + 1] ;
         uint8_t kb [BLOCK_LEN + 1] ;
 
@@ -121,7 +113,7 @@ namespace Safer {
         std::memset (kb, 0, sizeof (kb)) ;
     }
 
-    void        EncryptBlock (block_t &output, const block_t &input, const Table &tab, const Key &key) {
+    void        EncryptBlock (block_t &output, const block_t &input, const Table &tab, const Key &key) noexcept {
         uint8_t a, b, c, d, e, f, g, h ;
 
         a = input [0] ; b = input [1] ; c = input [2] ; d = input [3] ;
@@ -158,7 +150,7 @@ namespace Safer {
         output [7] = h ^ key [idx + 7] ;
     }
 
-    void    DecryptBlock (block_t &output, const block_t &input, const Table &tab, const Key &key) {
+    void    DecryptBlock (block_t &output, const block_t &input, const Table &tab, const Key &key) noexcept {
         uint8_t a, b, c, d, e, f, g, h ;
         size_t  idx = BLOCK_LEN * (1 + 2 * key.RoundCount ()) - 1 ;
 
@@ -195,6 +187,3 @@ namespace Safer {
         output [4] = e ; output [5] = f ; output [6] = g ; output [7] = h ;
     }
 }   /* end of namespace Safer */
-/*
- * [END of FILE]
- */
